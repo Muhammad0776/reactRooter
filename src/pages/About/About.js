36a651b0144f60f20d8807c8bc746162f0911f68
Mukhammad <81352000+Muhammad0776@users.noreply.github.com>
2021-07-29 10:02:34 +0500

    reactRooter

---
+++ src/pages/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./About.css"
+
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h1>About page</h1>
+        </div>
+    )
+}
+
+export default About;
